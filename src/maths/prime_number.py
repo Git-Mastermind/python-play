ace7a0cfaa14607578c9cc2822b6bb4a3929e99f
Eshan Jha <eshan-jha@outlook.com>
2026-01-04 19:40:24 -0800

    optimize prime_number, delete unit test for prime_number

--- src/maths/prime_number.py
+++ src/maths/prime_number.py
@@ -1,16 +1,36 @@
 from datetime import datetime
-def check_prime_number(num):  # RETURNS: Boolean for is_prime, and run time
-    start_time = datetime.now()  # starting the timer
-    factors = []  # empty list for factors
-    for i in range(1, num + 1):
-        if num % i == 0:  # if the number is a factor
-            factors.append(i)  # append to the factors list
-    end_time = datetime.now()  # after the loop runs, mark the end time
-    run_time = end_time - start_time
+import math
+def check_prime_number(num):  
+    start_time = datetime.now() 
+    is_prime = True
+    ending_point = math.ceil(math.sqrt(num)) + 1
+    #ending_point = 56893997
+    for i in range(2,  ending_point):
+        if num % i == 0: 
+            print(i)  
+            is_prime = False  # found one factor, cannot be prime
+            break
+    end_time = datetime.now()
+    run_time = end_time - start_time 
+    print(f"run time: {run_time.total_seconds() * 1000} ms")
+    
+    return is_prime
 
-    if factors == [1, num]:  # if the number is a prime number
-        return True, run_time  # return that it is a prime number and the run time
-    else:
-        return False, run_time  # return that it is not a prime number and the run time
+
+# BEFORE: 
+# 56893999 took 3327 ms
+# 56893990 took 3366 ms
+
+# AFTER:
+
+# 56893990 took 0.048 ms
+
+# BEFORE:
+# 56893997 took 3417 ms
+
+# AFTER
+# 56893997 took 0.425 ms
+
+print(check_prime_number(56893997))
 
 

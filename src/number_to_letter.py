ad9b70f180011adcab5ba76ad8749b24c17c31e4
Eshan Jha <eshan-jha@outlook.com>
2026-01-12 17:09:25 -0800

    finish number_to_letter and delete number_to_letter_2

--- src/number_to_letter.py
+++ src/number_to_letter.py
@@ -16,7 +16,8 @@ tens = {
     4:"fourty",
     5:"fifty",
     6:"sixty",
-    7:"eighty",
+    7:"seventy",
+    8:"eighty",
     9:"ninety"
 }
 
@@ -49,30 +50,39 @@ else:
         tens_place = tens[int(number[0])] + " " + base_numbers[int(number[1])]
         print(f"{tens_place}")
         quit()
+
+    elif len(number) == 3:
+        hundreds_place = number[0]
+        if int(number[1]) == 0:
+            tens_place = base_numbers[int(number[2])]
+        else:
+            if int(number[1]) < 2:
+                tens_place = teens[int(f"{number[1]}{number[2]}")]
+            else:
+                tens_place = tens[int(number[1])] + " " + base_numbers[int(number[2])]
+        print(f"{base_numbers[int(hundreds_place)]} hundred and {tens_place}")
     
     elif len(number) == 4:
-        if number[1] == 0:
-            if int(number[2]) < 2:
+        if int(number[1]) == 0:
+            if int(number[2]) == 1:
                 tens_place = teens[int(f"{number[2]}{number[3]}")]
+            elif int(number[2]) == 0:
+                tens_place = base_numbers[int(number[3])]
             else:
                 tens_place = tens[int(number[2])] + " " + base_numbers[int(number[3])]
             print(f"{thousands_place} thousand and {tens_place}")
+        elif int(number[2]) == 0:
+            hundreds_place = number[1]
+            tens_place = base_numbers[int(number[3])]
+            print(f"{thousands_place} thousand {base_numbers[int(hundreds_place)]} hundred and {tens_place}")
         else:
             hundreds_place = number[1]
             if int(number[2]) < 2:
                 tens_place = teens[int(f"{number[2]}{number[3]}")]
             else:
                 tens_place = tens[int(number[2])] + " " + base_numbers[int(number[3])]
-                print(f"{thousands_place} thousand {hundreds_place} hundred and {tens_place}")
+                print(f"{thousands_place} thousand {base_numbers[int(hundreds_place)]} hundred and {tens_place}")
 
             
         quit()
 
-    if int(number[1]) == 0:
-        tens_place = base_numbers[int(number[2])]
-    else:
-        if int(number[1]) < 2:
-            tens_place = teens[int(f"{number[1]}{number[2]}")]
-        else:
-            tens_place = tens[int(number[1])] + " " + base_numbers[int(number[2])]
-    print(f"{hundreds_place} hundred and {tens_place}")

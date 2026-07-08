b0ade2903a2ed4de48faa8f8ce70f2e4fc67865e
Git-Mastermind <eshan-jha@outlook.com>
2026-07-07 20:34:08 -0700

    initiate flask sever and write new-entry route

--- com/wifi_app/main_flask.py
+++ com/wifi_app/main_flask.py
@@ -1,3 +1,23 @@
-from flask import Flask
+from flask import Flask, request
 import mysql.connector
 import requests
+from user_credentials import HOST, USER, PASSWORD, DATABASE
+
+conn = mysql.connector.connect(
+    host=HOST,
+    user=USER,
+    password=PASSWORD,
+    database=DATABASE
+)
+
+app = Flask(__name__)
+
+@app.route("/new-entry", methods = ["POST"])
+
+def new_wifi_entry():
+    cursor = conn.cursor()
+    name = request.args.get("name")
+    password = request.args.get("password")
+
+    cursor.execute("INSERT INTO wifi_entries (name, password, downvotes) VALUES (%s, %s, %s);", (name, password, 0))
+    

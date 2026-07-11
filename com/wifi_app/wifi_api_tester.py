1ea18dd0716c0d1c0d9abf94538037fd18659693
Git-Mastermind <eshan-jha@outlook.com>
2026-07-11 13:54:04 -0700

    make new wifi section

--- com/wifi_app/wifi_api_tester.py
+++ com/wifi_app/wifi_api_tester.py
@@ -1,4 +1,19 @@
 import streamlit as st
+import requests
+import time
 
-operation = st.radio("", (""))
+operation = st.segmented_control("", ["new wifi", "search", "get wifis", "downvote"])
+if operation:
+    if operation == "new wifi":
+        name = st.text_input("Name")
+        password = st.text_input("Password")
+        if name and password:
+            new_wifi_entry_url = f"http://127.0.0.1:5000/new-entry?name={name}&password={password}"
+            response = requests.post(new_wifi_entry_url)
+            time.sleep(1.5)
+            if response.status_code == 200 or response.status_code == 201:
+                st.write(f"✅ wifi successfully created")
+            else:
+                st.write(f"❌ error in creating wifi")
+        
 
